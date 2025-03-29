5409a9b20c9c75d7542093768a5e2f99cbe8b9a8
AlexanderHagenhoff <hagenhoff.alexander@gmail.com>
2025-03-29 23:59:58 +0100

    cv-008: Cleanup sorting of images by height

--- src/main.py
+++ src/main.py
@@ -2,19 +2,25 @@ from image_composition.html_image_composer import HtmlImageComposer
 from image_generation.code_image_generator import CodeImageGenerator
 from src.code_handling.file_system_loader import FileSystemLoader
 
+def sort_images_by_height(images_with_files):
+    decorated = [(image.height, image, file) for image, file in images_with_files]
+    decorated.sort(reverse=True, key=lambda x: x[0])
+    return [(image, file) for height, image, file in decorated]
+
 if __name__ == "__main__":
     directory = r'D:\dev\spring-boot\spring-boot-project\spring-boot-actuator'
-
-    urls = [
-        "https://github.com/AlexanderHagenhoff/spring-boot-postgres-archetype"
-    ]
-
-    output_image_path = "output_image.png"
 
     loader = FileSystemLoader()
     found_files = loader.load_code_files([directory], ["*.java", "pom.xml"])
 
     generator = CodeImageGenerator(400, 300, 3, 5)
+
+    images_with_files = []
+    for file in found_files:
+        image = generator.generate_image(file)
+        images_with_files.append((image, file))  # Nur (image, file) speichern
+
+    sorted_images = sort_images_by_height(images_with_files)
 
     composer = HtmlImageComposer(
         output_directory="../output/html_output",
@@ -22,8 +28,7 @@ if __name__ == "__main__":
         thumbnail_size=(500, 1000)
     )
 
-    for file in found_files:
-        image = generator.generate_image(file)
+    for image, file in sorted_images:
         composer.add_image(image, file)
 
     composer.generate_html()
